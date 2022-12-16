6c4e74a4460de96746452a8bbc55d03be9aa2843
holgadoM <holgado.m@gmail.com>
2022-12-16 12:57:33 -0300

    fix logs

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-
-const { ZIPKIN_LOCAL_ENDPOINT, ZIPKIN_SERVICE_NAME } = require('./app/common/constants')
+const { Kafka } = require('kafkajs')
+const { Pool } = require('pg')
 
 const NacosConfigClient = require('nacos').NacosConfigClient;
 
@@ -20,35 +20,23 @@ async function startServer() {
             for (const key in config) {
                 process.env[key.toUpperCase()] = config[key];
             }
+
+            serRoutesAndListener()
             return config;
         }).catch((err)=>{
             console.log('Error con nacos');
             console.log(err);
         })
           
-      
-    const PORT = process.env.SERVER_PORT_ACCOUNT || 3002;
+    init_kafka_consumer();
+    
+}
 
-    const { Tracer, ExplicitContext, BatchRecorder, jsonEncoder } = require('zipkin')
-    const { HttpLogger } = require('zipkin-transport-http')
-    const zipkinMiddleware = require('zipkin-instrumentation-express').expressMiddleware
-    const ZIPKIN_ENDPOINT = process.env.ZIPKIN_ENDPOINT || ZIPKIN_LOCAL_ENDPOINT
-    const tracer = new Tracer({
-        ctxImpl: new ExplicitContext(),
-        recorder: new BatchRecorder({
-            logger: new HttpLogger({
-                endpoint: `${ZIPKIN_ENDPOINT}/api/v2/spans`,
-                jsonEncoder: jsonEncoder.JSON_V2,
-            }),
-        }),
-        localServiceName: ZIPKIN_SERVICE_NAME,
-    })
-    app.use(zipkinMiddleware({ tracer }))
+function serRoutesAndListener(){
+    const PORT = process.env.SERVER_PORT_ACCOUNT || 3002;
     
     app.use(express.json())
     app.use('/api', require('./app/routes'))
-
-    init_kafka_consumer();
     
     app.listen(PORT, () => {
         console.log('Application running on port ', PORT)
@@ -56,50 +44,60 @@ async function startServer() {
 }
 
 async function init_kafka_consumer(){
-    const kafka = new Kafka({
-        clientId: 'pay-client',
-        brokers: [process.env.KAFKA_SERVER],
-    });
 
-    const pool = new Pool({
-        user: process.env.DB_POSTGRES_USER,
-        password: process.env.DB_POSTGRES_PASSWORD,
-        database: process.env.DB_POSTGRES_DATABASE_ACCOUNT,
-        host: process.env.DB_POSTGRES_HOST,
-        port: process.env.DB_POSTGRES_PORT,
-        ssl: { 
-            rejectUnauthorized: !Boolean(process.env.DB_POSTGRES_REJECTUNAUTHORIZED),
-        },
-        dialect: process.env.DB_POSTGRES_DIALECT,
-    });
+    const logProvider = require('./app/middleware/logprovider')
 
-    console.log('Iniciando kafka_consumer...');
-
-    const consumer = kafka.consumer({groupId: 'pay-subcription', allowAutoTopicCreation: true});
-    await consumer.connect();
-    await consumer.subscribe({topic: 'pay-topic', fromBeginning: true });
-    await consumer.run({
-        autoCommit: false,
-        eachMessage: async ({topic, partition, message})=>{
-            console.log({ value: message.value.toString() })
-
-            var jsonObj = JSON.parse(message.value.toString())
-            var amountNew = 0;
-            if( jsonObj.type === 'pay' ){
-                amountNew = jsonObj.amount * (-1)
-            }else{
-                amountNew = jsonObj.amount;
-            }
-
-            pool.query('UPDATE account SET amount = amount + $1 WHERE id= $2',[amountNew, jsonObj.accountId], async (err, result)=>{
-                if( err ){
-                    return console.log('Error executing query', err.stack)
+    try {
+        logProvider.info('Iniciando kafka-invoice');
+        const kafka = new Kafka({
+            clientId: 'pay-client',
+            brokers: [process.env.KAFKA_SERVER],
+        });
+    
+        const pool = new Pool({
+            user: process.env.DB_POSTGRES_USER,
+            password: process.env.DB_POSTGRES_PASSWORD,
+            database: process.env.DB_POSTGRES_DATABASE_ACCOUNT,
+            host: process.env.DB_POSTGRES_HOST,
+            port: process.env.DB_POSTGRES_PORT,
+            ssl: { 
+                rejectUnauthorized: !Boolean(process.env.DB_POSTGRES_REJECTUNAUTHORIZED),
+            },
+            dialect: process.env.DB_POSTGRES_DIALECT,
+        });
+    
+        const consumer = kafka.consumer({groupId: 'pay-subcription', allowAutoTopicCreation: true});
+        await consumer.connect();
+        await consumer.subscribe({topic: 'pay-topic', fromBeginning: true });
+        await consumer.run({
+            autoCommit: false,
+            eachMessage: async ({topic, partition, message})=>{
+    
+                var jsonObj = JSON.parse(message.value.toString())
+                var amountNew = 0;
+                if( jsonObj.type === 'pay' ){
+                    amountNew = jsonObj.amount * (-1)
+                }else{
+                    amountNew = jsonObj.amount;
                 }
-                console.log('Account modifield with accountId: ', jsonObj.accountId)
-                await consumer.commitOffsets([{topic, partition, offset: ( Number(message.offset)+1).toString() }])
-            })
-        }
-    })
+    
+                pool.query('UPDATE account SET amount = amount + $1 WHERE id= $2',[amountNew, jsonObj.accountId], async (err, result)=>{
+                    if( err ){
+                        logProvider.info('Error executing query', err.stack)
+                        return;
+                    }
+                    logProvider.info('Account modifield with accountId: ', jsonObj.accountId)
+                    await consumer.commitOffsets([{topic, partition, offset: ( Number(message.offset)+1).toString() }])
+                })
+            }
+        })
+        
+    } catch (error) {
+        console.log('Error kafka');
+        console.log(error);
+        
+    }
+
 }
 
 startServer()
